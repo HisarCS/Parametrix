e381c3ec365c07a661862ec47b05fffbf4b7a9ac
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2024-12-19 13:19:37 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -36,7 +36,7 @@ function renderHTML() {
         <head>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>F360LLM</title>
+          <title>Parametrix</title>
           <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
           <style>
             body {
@@ -105,7 +105,7 @@ function renderHTML() {
           </style>
         </head>
         <body>
-          <h1><span>F360</span>LLM</h1>
+          <h1><span>PARAMETRI</span>X</h1>
           <button class="start-button" onclick="loadLevelSelection()">Start</button>
           <p class="start-message">Press Enter to Start</p>
           <div class="footer">.)</div>
