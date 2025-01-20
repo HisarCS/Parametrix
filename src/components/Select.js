48cfd6a4df6cebcb0a962f72dc3fd2274fa115b8
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2025-01-20 20:35:27 +0300

    wildcard

--- src/components/Select.js
+++ src/components/Select.js
@@ -18,7 +18,7 @@ const Select = () => {
         textAlign: "center",
       }}
     >
-      <h1 style={{ fontSize: "2.5rem", marginBottom: "30px" }}>Select a Level</h1>
+      <h1 style={{ fontSize: "2.5rem", marginBottom: "200px" }}>Select a Level</h1>
       <div
         style={{
           display: "grid",
@@ -28,7 +28,7 @@ const Select = () => {
           maxWidth: "600px",
         }}
       >
-        {[1, 2, 3, 4, 5, 6,7,8,9].map((level) => (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((level) => (
           <button
             key={level}
             style={{
@@ -45,7 +45,7 @@ const Select = () => {
               fontFamily: "'Press Start 2P', cursive",
               transition: "transform 0.2s, box-shadow 0.2s",
             }}
-            onClick={() => navigate(`/level/${level}`)}
+            onClick={() => navigate(`/lvl${level}`)}
             onMouseOver={(e) => {
               e.target.style.boxShadow = "0 0 10px white";
               e.target.style.transform = "scale(1.1)";
@@ -59,14 +59,43 @@ const Select = () => {
           </button>
         ))}
       </div>
+
+      {/* Back Button */}
       <button
-        onClick={() => navigate(-1)} // Go back to the previous page
+        onClick={() => navigate(-1)}
         style={{
           marginTop: "40px",
           padding: "10px 20px",
           fontSize: "1.2rem",
           color: "white",
           backgroundColor: "transparent",
+          border: "2px solid white",
+          cursor: "pointer",
+          textTransform: "uppercase",
+          fontFamily: "'Press Start 2P', cursive",
+          transition: "transform 0.2s, box-shadow 0.2s",
+          marginBottom: "200px",
+        }}
+        onMouseOver={(e) => {
+          e.target.style.boxShadow = "0 0 10px white";
+          e.target.style.transform = "scale(1.1)";
+        }}
+        onMouseOut={(e) => {
+          e.target.style.boxShadow = "none";
+          e.target.style.transform = "scale(1)";
+        }}
+      >
+        Back
+      </button>
+
+      {/* Wildcard Button */}
+      <button
+        style={{
+          marginTop: "0px", // Further below
+          padding: "5px 10px",
+          fontSize: "1.5rem",
+          color: "white",
+          backgroundColor: "#af8636",
           border: "2px solid white",
           cursor: "pointer",
           textTransform: "uppercase",
@@ -82,7 +111,7 @@ const Select = () => {
           e.target.style.transform = "scale(1)";
         }}
       >
-        Back
+        Wildcard
       </button>
     </div>
   );
